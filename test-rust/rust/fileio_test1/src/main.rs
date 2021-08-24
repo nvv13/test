d0b1f6f29834b5cde10b8ea1b564fab3df828e9f
nvv13 <nvv13@mail.ru>
2021-08-24 20:22:36 +0300

    2

--- test-rust/rust/fileio_test1/src/main.rs
+++ test-rust/rust/fileio_test1/src/main.rs
@@ -3,40 +3,33 @@ use std::io::prelude::*;
 use std::io::LineWriter;
 
 fn main() -> std::io::Result<()> {
-    let road_not_taken = b"I shall be telling this with a sigh
-Somewhere ages and ages hence:
-Two roads diverged in a wood, and I -
-I took the one less traveled by,
-And that has made all the difference.";
+    let road_not_taken = b"l,tl,l,
+555
+yyyu
+gtgtg
+.";
 
-    let file = File::create("poem.txt")?;
+    let file = File::create("text1.txt")?;
     let mut file = LineWriter::new(file);
 
-    file.write_all(b"I shall be telling this with a sigh")?;
+    file.write_all(b"test test")?;
 
-    // No bytes are written until a newline is encountered (or
-    // the internal buffer is filled).
-    assert_eq!(fs::read_to_string("poem.txt")?, "");
+    //
+    assert_eq!(fs::read_to_string("text1.txt")?, "");
     file.write_all(b"\r\n")?;
-    assert_eq!(
-        fs::read_to_string("poem.txt")?,
-        "I shall be telling this with a sigh\r\n",
-    );
+    assert_eq!(fs::read_to_string("text1.txt")?, "test test\r\n",);
 
-    println!("read_to_string {}",fs::read_to_string("poem.txt")?);
-    // Write the rest of the poem.
-    file.write_all(b"Somewhere ages and ages hence:
-Two roads diverged in a wood, and I -
-I took the one less traveled by,
-And that has made all the difference.")?;
+    println!("read_to_string {}", fs::read_to_string("text1.txt")?);
+    // Write
+    file.write_all(
+        b"123 ghyyy
+mmmm tttht
+tttttttttttttttttt.",
+    )?;
 
-    // The last line of the poem doesn't end in a newline, so
-    // we have to flush or drop the `LineWriter` to finish
-    // writing.
     file.flush()?;
 
-    // Confirm the whole poem was written.
-    assert_eq!(fs::read("poem.txt")?, &road_not_taken[..]);
+    // Confirm write
+    assert_eq!(fs::read("text1.txt")?, &road_not_taken[..]);
     Ok(())
 }
-
