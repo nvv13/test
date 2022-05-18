6d938f6fad768ed65cb2f502033d97e74062a934
nvv13 <nvv13@mail.ru>
2022-05-18 11:23:51 +0300

    sync2

---
+++ test-mk/w801/sdk-prj/01_blink/app/main.c
@@ -0,0 +1,34 @@
+/***************************************************************************** 
+* 
+* File Name : main.c
+* 
+* Description: main 
+* 
+* Copyright (c) 2014 Winner Micro Electronic Design Co., Ltd. 
+* All rights reserved. 
+* 
+* Author : dave
+* 
+* Date : 2014-6-14
+*****************************************************************************/ 
+
+#include "wm_gpio.h"
+
+void UserMain(void)
+{
+	printf("user task\n");
+	
+	tls_gpio_cfg(WM_IO_PB_05, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
+	tls_gpio_cfg(WM_IO_PB_11, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
+	
+	u8 u8_led_state=0;
+	while(1)
+	{
+	    tls_gpio_write(WM_IO_PB_05, u8_led_state);	
+	    tls_gpio_write(WM_IO_PB_11, u8_led_state);
+	    tls_os_time_delay(200);
+	    u8_led_state=~u8_led_state;
+	}
+
+}
+
---
+++ test-mk/w801/sdk-prj/02_task_blink/app/main.c
@@ -0,0 +1,77 @@
+/***************************************************************************** 
+* 
+* File Name : main.c
+* 
+* Description: main 
+* 
+* Copyright (c) 2014 Winner Micro Electronic Design Co., Ltd. 
+* All rights reserved. 
+* 
+* Author : dave
+* 
+* Date : 2014-6-14
+*****************************************************************************/ 
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include "wm_type_def.h"
+//#include "wm_uart.h"
+#include "wm_gpio.h"
+//#include "wm_hostspi.h"
+//#include "wm_socket.h"
+//#include "wm_sockets.h"
+//#include "wm_wifi.h"
+//#include "wm_hspi.h"
+//#include "wm_pwm.h"
+//#include "wm_params.h"
+#include "wm_osal.h"
+//#include "wm_netif.h"
+//#include "wm_efuse.h"
+//#include "wm_mem.h"
+//#include "wm_regs.h"
+
+
+
+#define    DEMO_TASK_SIZE      2048
+static OS_STK 			DemoTaskStk[DEMO_TASK_SIZE];
+#define  DEMO_TASK_PRIO			                32
+
+
+//console task use UART0 as communication port with PC
+void demo_console_task(void *sdata)
+{
+
+    u8 u8_led_state=0;
+
+    for(;;)
+    {
+	    tls_gpio_write(WM_IO_PB_05, u8_led_state);	
+	    tls_gpio_write(WM_IO_PB_11, u8_led_state);
+	    tls_os_time_delay(100);
+	    u8_led_state=~u8_led_state;
+    }
+}
+
+void UserMain(void)
+{
+	printf("user task\n");
+	
+	tls_gpio_cfg(WM_IO_PB_05, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
+	tls_gpio_cfg(WM_IO_PB_11, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
+
+
+    tls_os_task_create(NULL, NULL,
+                       demo_console_task,
+                       NULL,
+                       (void *)DemoTaskStk,          /* task's stack start address */
+                       DEMO_TASK_SIZE * sizeof(u32), /* task's stack size, unit:byte */
+                       DEMO_TASK_PRIO,
+                       0);
+
+	
+//	while(1)
+//	{
+//	}
+
+}
+
