2de79c3bc409e81dd039536f2fcdd9a66a016ac9
nvv13 <nvv13@mail.ru>
2022-11-09 22:12:27 +0300

    sync2

--- test-mk/w801/sdk-prj/01_blink_PMU/app/main.c
+++ test-mk/w801/sdk-prj/01_blink_PMU/app/main.c
@@ -1,44 +1,48 @@
 #include "wm_type_def.h"
 
-//#include "wm_watchdog.h"
-
-#include <string.h>
-#include "wm_include.h"
-#include "wm_demo.h"
 #include "wm_pmu.h"
+#include "wm_gpio.h"
 
+#include <stdio.h>
+#include <string.h>
 
-static void pmu_timer1_irq(u8 *arg)
+volatile static u8 u8_led_state = 0;
+
+static void
+pmu_timer1_irq (u8 *arg)
 {
-    tls_pmu_timer1_stop();
-    printf("pmu timer1 irq\n");
+  tls_pmu_timer1_stop ();
+  u8_led_state = ~u8_led_state;
+  printf ("pmu timer1 irq\n");
 }
-
-
-int pmu_timer1_demo(u8 enterStandby)
-{
-	tls_pmu_clk_select(0);/*0:select 32K RC osc, 1: select 40M divide clock*/
-    tls_pmu_timer1_isr_register((tls_pmu_irq_callback)pmu_timer1_irq, NULL);
-    tls_pmu_timer1_start(5000);
-    printf("pmu timer1 test start\n");
-
-    if(enterStandby)
-    {
-        printf("pmu will standby\n");
-        tls_pmu_standby_start();/*If you want to verify sleep function, using function tls_pmu_sleep_start()*/
-        printf("pmu enter standby\n");
-    }
-    return 	WM_SUCCESS;
-}
-
-
 
 void
 UserMain (void)
 {
 
-//  tls_watchdog_init (12 * 1000
-//                     * 1000); // u32 usec около 6 сек --около 1-2 минуты
+  tls_gpio_cfg (WM_IO_PB_05, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
+  tls_gpio_cfg (WM_IO_PB_11, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
+  tls_gpio_cfg (WM_IO_PB_16, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
+  tls_gpio_cfg (WM_IO_PB_25, WM_GPIO_DIR_OUTPUT, WM_GPIO_ATTR_FLOATING);
 
+  tls_pmu_clk_select (0); /*0:select 32K RC osc, 1: select 40M divide clock*/
+  tls_pmu_timer1_isr_register ((tls_pmu_irq_callback)pmu_timer1_irq, NULL);
 
+  for (;;)
+    {
+
+      tls_pmu_timer1_start (5000);
+      printf ("pmu timer1 test start\n");
+
+      printf ("pmu will sleep\n");
+      tls_pmu_sleep_start (); /* tls_pmu_standby_start ()  If you want to
+                                 verify sleep function, using function
+                                 tls_pmu_sleep_start()*/
+      printf ("pmu enter sleep\n");
+
+      tls_gpio_write (WM_IO_PB_05, u8_led_state);
+      tls_gpio_write (WM_IO_PB_11, u8_led_state);
+      tls_gpio_write (WM_IO_PB_16, ~u8_led_state);
+      tls_gpio_write (WM_IO_PB_25, ~u8_led_state);
+    }
 }
