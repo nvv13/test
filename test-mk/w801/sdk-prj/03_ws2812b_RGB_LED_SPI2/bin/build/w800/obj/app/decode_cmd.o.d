fe73acaf2ae1a2f4611d18ea713a9001ac8fce50
nvv13 <nvv13@mail.ru>
2022-12-23 15:00:24 +0300

    sync2

--- test-mk/w801/sdk-prj/03_ws2812b_RGB_LED_SPI2/bin/build/w800/obj/app/decode_cmd.o.d
+++
@@ -1,7 +0,0 @@
-../bin/build/w800/obj/app/decode_cmd.o: decode_cmd.c decode_cmd.h \
- ../src/app/ota/wm_http_fwup.h ../include/wm_type_def.h \
- ../include/app/wm_http_client.h ../include/wm_config.h \
- ../include/arch/xt804/csi_config.h ../include/os/wm_os_config.h \
- ../include/wm_wifi_config.h ../include/wm_ram_config.h \
- ../include/wm_config.h el_cmd.h w_flash_cfg.h ../include/driver/wm_rtc.h \
- ws2812b.h color.h ../include/driver/wm_gpio.h ../include/driver/wm_io.h
--- test-mk/w801/sdk-prj/03_ws2812b_RGB_LED_SPI2/bin/build/w800/obj/app/n_delay.o.d
+++
@@ -1,6 +0,0 @@
-../bin/build/w800/obj/app/n_delay.o: n_delay.c n_delay.h \
- ../include/arch/xt804/csi_core/csi_core.h \
- ../include/arch/xt804/csi_core/core_804.h \
- ../include/arch/xt804/csi_config.h \
- ../include/arch/xt804/csi_core/csi_gcc.h ../include/wm_type_def.h \
- ../include/driver/wm_cpu.h
