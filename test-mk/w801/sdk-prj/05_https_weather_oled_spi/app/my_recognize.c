ddc0ee1cdb511cad2278965030ddff6c3ed726f1
nvv13 <nvv13@mail.ru>
2023-03-08 15:50:18 +0300

    sync2

---
+++ test-mk/w801/sdk-prj/05_https_weather_oled_spi/app/my_recognize.c
@@ -0,0 +1,243 @@
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "wm_type_def.h"
+
+#include "my_recognize.h"
+
+static int i_temperature_c = MY_RECOGNIZE_NO_VALUE;
+static int i_sign = 1;
+static int i_temperature_mantissa_c = 0;
+static struct tm t_last_query;
+
+struct tm
+my_recognize_ret_t_last_query (void)
+{
+  return t_last_query;
+}
+int
+my_recognize_ret_cur_temperature (void)
+{
+  return i_temperature_c;
+}
+int
+my_recognize_ret_cur_temperature_sign (void)
+{
+  return i_sign;
+}
+int
+my_recognize_ret_cur_temperature_mantissa (void)
+{
+  return i_temperature_mantissa_c;
+}
+
+enum
+{
+  RG_START_FIND = 0,
+  RG_FIND_PRE2,
+  RG_FIND_PRE3,
+  RG_FIND_PRE3_M,
+  RG_FIND_PRE4,
+  RG_FIND_PRE5,
+  RG_FIND_PRE6,
+  RG_FIND_END
+};
+
+static u8 u8_status_find = RG_START_FIND;
+
+char *c_PRE1 = "weather-value";
+int i_POS_PRE1 = 0;
+
+char *c_PRE2 = "unit_temperature_c";
+int i_POS_PRE2 = 0;
+
+char *c_PRE3 = "sign\">";
+int i_POS_PRE3 = 0;
+
+char *c_PRE4 = "</span>";
+int i_POS_PRE4 = 0;
+
+char buf_temperatura[10];
+u8 u8_buf_pos_temperatura = 0;
+
+char *c_PRE5 = "<";
+int i_POS_PRE5 = 0;
+
+char *c_PRE6 = "temperature_f";
+int i_POS_PRE6 = 0;
+
+char *c_PRE7 = "lower\">,";
+int i_POS_PRE7 = 0;
+
+void
+my_recognize_http_reset (void)
+{
+  if (u8_status_find != RG_FIND_END)
+    my_recognize_http_error ();
+  //
+  u8_status_find = RG_START_FIND;
+  i_POS_PRE1 = 0;
+  i_POS_PRE2 = 0;
+  i_POS_PRE3 = 0;
+  i_POS_PRE4 = 0;
+  i_POS_PRE5 = 0;
+  i_POS_PRE6 = 0;
+  i_POS_PRE7 = 0;
+  u8_buf_pos_temperatura = 0;
+}
+
+void
+my_recognize_http_error (void)
+{
+  my_recognize_http_reset ();
+  i_temperature_c = MY_RECOGNIZE_NO_VALUE;
+  i_sign = 1;
+  i_temperature_mantissa_c = 0;
+}
+
+void
+my_recognize_http (const char *recvbuf, int i_len)
+{
+  for (int iInd = 0; iInd < i_len; iInd++)
+    {
+      char ch = *(recvbuf + iInd);
+      switch (u8_status_find)
+        {
+        case RG_START_FIND:
+          {
+            if (ch == c_PRE1[i_POS_PRE1]) // "weather-value";
+              i_POS_PRE1++;
+            else
+              {
+                if (i_POS_PRE1 < strlen (c_PRE1))
+                  i_POS_PRE1 = 0;
+                else
+                  u8_status_find = RG_FIND_PRE2;
+              }
+          };
+          break;
+
+        case RG_FIND_PRE2:
+          {
+            if (ch
+                == c_PRE2[i_POS_PRE2]) // "unit_temperature_c"; span
+                                       // class="unit unit_temperature_c"><span
+                                       // class="sign">&minus;</span>4<span
+                                       // class="lower">,3</span>
+              i_POS_PRE2++;
+            else
+              {
+                if (i_POS_PRE2 < strlen (c_PRE2))
+                  i_POS_PRE2 = 0;
+                else
+                  u8_status_find = RG_FIND_PRE3;
+              }
+          };
+          break;
+
+        case RG_FIND_PRE3:
+          {
+            if (ch == c_PRE3[i_POS_PRE3]) // "sign\">";   ="sign">+</span> ,
+                                          // грузим знак, если есть и дальше...
+              i_POS_PRE3++;
+            else
+              {
+                if (i_POS_PRE3 < strlen (c_PRE3))
+                  i_POS_PRE3 = 0;
+                else
+                  {
+                    if (ch == '-')
+                      i_sign = -1;
+                    else
+                      i_sign = 1;
+                    u8_status_find = RG_FIND_PRE4;
+                    if (ch == '&')
+                      u8_status_find = RG_FIND_PRE3_M;
+                  }
+              }
+          };
+          break;
+
+        case RG_FIND_PRE3_M:
+          {
+            if (ch == 'm')
+              i_sign = -1;
+            else
+              i_sign = 1;
+            u8_status_find = RG_FIND_PRE4;
+          };
+          break;
+
+        case RG_FIND_PRE4:
+          {
+            if (ch == c_PRE4[i_POS_PRE4]) // "</span>"; далее идёт число -
+                                          // температура  </span>20<span
+              i_POS_PRE4++;
+            else
+              {
+                if (i_POS_PRE4 < strlen (c_PRE4))
+                  i_POS_PRE4 = 0;
+                else
+                  {
+                    buf_temperatura[u8_buf_pos_temperatura++] = ch;
+                    u8_status_find = RG_FIND_PRE5;
+                  }
+              }
+          };
+          break;
+
+        case RG_FIND_PRE5:
+          {
+            if (ch == c_PRE5[i_POS_PRE5]) // "<";
+              i_POS_PRE5++;
+            else
+              {
+                if (i_POS_PRE5 < strlen (c_PRE5)
+                    && u8_buf_pos_temperatura < 10)
+                  {
+                    i_POS_PRE5 = 0;
+                    buf_temperatura[u8_buf_pos_temperatura++] = ch;
+                  }
+                else
+                  u8_status_find = RG_FIND_PRE6;
+              }
+          };
+          break;
+
+        case RG_FIND_PRE6:
+          {
+            if (ch == c_PRE6[i_POS_PRE6]) // "temperature_f"; end
+              i_POS_PRE6++;
+            else
+              {
+                if (i_POS_PRE6 < strlen (c_PRE6)
+                    && u8_buf_pos_temperatura < 10)
+                  i_POS_PRE6 = 0;
+                else
+                  {
+                    buf_temperatura[u8_buf_pos_temperatura++] = 0x00;
+                    i_temperature_c = atoi (buf_temperatura);
+                    u8_status_find = RG_FIND_END;
+                    tls_get_rtc (&t_last_query);
+                  }
+              }
+
+            if (ch == c_PRE7[i_POS_PRE7]) // "lower\">,";
+              i_POS_PRE7++;
+            else
+              {
+                if (i_POS_PRE7 < strlen (c_PRE7))
+                  i_POS_PRE7 = 0;
+                else
+                  {
+                    if (i_POS_PRE7 == strlen (c_PRE7))
+                      i_temperature_mantissa_c = ch - 0x30;
+                    i_POS_PRE7++;
+                  }
+              }
+          };
+          break;
+        }
+    }
+}
---
+++ test-mk/w801/sdk-prj/05_https_weather_oled_spi/app/w_flash_cfg.c
@@ -0,0 +1,80 @@
+
+
+#include "wm_type_def.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "wm_uart.h"
+#include "wm_gpio.h"
+//#include "wm_hostspi.h"
+//#include "wm_socket.h"
+//#include "wm_sockets.h"
+//#include "wm_wifi.h"
+//#include "wm_hspi.h"
+//#include "wm_pwm.h"
+//#include "wm_params.h"
+#include "wm_osal.h"
+//#include "wm_netif.h"
+//#include "wm_efuse.h"
+#include "wm_mem.h"
+#include "wm_regs.h"
+#include "wm_rtc.h"
+#include "wm_timer.h"
+//#include "wm_watchdog.h"
+//#include "wm_cpu.h"
+//#include "csi_core.h"
+
+#include "w_flash_cfg.h"
+#include "wm_internal_flash.h"
+
+#define TEST_FLASH_BUF_SIZE 512
+
+int
+flash_cfg_load_u16 (u16 *u16_value, u8 u8_index)
+{
+  u8 *read_buf = NULL;
+
+  tls_fls_init (); // initialize flash driver
+
+  read_buf = tls_mem_alloc (TEST_FLASH_BUF_SIZE);
+  if (NULL == read_buf)
+    {
+      printf ("\nmalloc read buf error\n");
+      return WM_FAILED;
+    }
+  memset (read_buf, 0, TEST_FLASH_BUF_SIZE);
+  tls_fls_read (0x1F0303, read_buf, TEST_FLASH_BUF_SIZE);
+
+  (*u16_value) = ((u16 *)read_buf)[u8_index];
+
+  tls_mem_free (read_buf);
+
+  return WM_SUCCESS;
+}
+
+int
+flash_cfg_store_u16 (u16 u16_value, u8 u8_index)
+{
+
+  u8 *write_buf = NULL;
+
+  tls_fls_init (); // initialize flash driver
+
+  write_buf = tls_mem_alloc (TEST_FLASH_BUF_SIZE);
+  if (NULL == write_buf)
+    {
+      printf ("\nmalloc write buf error\n");
+      return WM_FAILED;
+    }
+
+  memset (write_buf, 0, TEST_FLASH_BUF_SIZE);
+  tls_fls_read (0x1F0303, write_buf, TEST_FLASH_BUF_SIZE);
+
+  ((u16 *)write_buf)[u8_index] = u16_value;
+
+  tls_fls_write (0x1F0303, write_buf, TEST_FLASH_BUF_SIZE);
+
+  tls_mem_free (write_buf);
+
+  return WM_SUCCESS;
+}
