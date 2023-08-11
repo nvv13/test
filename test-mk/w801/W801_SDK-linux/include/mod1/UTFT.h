21b915f019df12e1e725477ed0844c042a6aa0c7
nvv13 <nvv13@mail.ru>
2023-08-11 18:44:09 +0300

    sync2

--- test-mk/w801/W801_SDK-linux/include/mod1/UTFT.h
+++ test-mk/w801/W801_SDK-linux/include/mod1/UTFT.h
@@ -144,6 +144,7 @@ extern "C"
 #define TFT02_0V89 44
 #define TFT_397T_NT35510 45
 #define TFT3_2SP_9341 46
+#define TFT3_5SP_9488 47
 
 #define ITDB32 0 // HX8347-A (16bit)
 #define ITDB32WC 1 // ILI9327  (16bit)
