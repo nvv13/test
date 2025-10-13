e26d72030a43e0bc75f925dd8ff511a512036e6d
nvv13 <nvv13@mail.ru>
2025-10-13 12:19:47 +0300

    sync2

--- test-mk/avr/attiny13a/01_blink3/main.c
+++ test-mk/avr/attiny13a/01_blink3/main.c
@@ -6,38 +6,113 @@
 //                        GND 4*****5 PB0 (MOSI/AIN0/OC0A/PCINT0)
 // ********************************************************************
 
-
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define OUT_LED1 PB1
+#define OUT_LED2 PB4
+#define IN_BUTTON PB3
 
+void
+my_delay_us (int i_cnt)
+{
+  while (i_cnt--)
+    _delay_us (1);
+}
+
+#define MAX_SWITCH 2
 int
-main(void)
+main (void)
 {
 
   /* setup */
-  DDRB |= (1 << PB4); // set pin as OUTPUT
-  DDRB |= (1 << PB3);
+  DDRB |= (1 << OUT_LED1); // set pin as OUTPUT
+  DDRB |= (1 << OUT_LED2);
 
-  int i_cnt=0;
+  DDRB &= ~(1 << IN_BUTTON); // set pin as INPUT
+  PORTB |= (1 << IN_BUTTON); // HiGh IN
+
+  int i_cnt = 0;
+  int i_but_delay = 0;
+  int i_switch = 0;
   /* loop */
   while (1)
     {
-      PORTB |= (1 << PB4); 
-      PORTB &= ~(1 << PB3);
-      if(i_cnt<500)
-        _delay_ms (1);       
-        else
-        _delay_ms (3);       
-      PORTB |= (1 << PB3); 
-      PORTB &= ~(1 << PB4); 
-      if(i_cnt<500)
-        _delay_ms (3);       
-        else
-        _delay_ms (1);       
-      if(i_cnt++>1000)
-         i_cnt=0;
+      if ((PINB & (1 << IN_BUTTON)) == 0 && i_but_delay == 0) // Down!
+        {
+          i_but_delay = 100;
+          i_cnt = 0;
+          if (++i_switch > MAX_SWITCH)
+            i_switch = 0;
+        }
+
+      switch (i_switch)
+        {
+
+        case 0:
+          {
+            if (++i_cnt > 500)
+              {
+                PORTB |= (1 << OUT_LED1);
+                PORTB &= ~(1 << OUT_LED2);
+                my_delay_us (1000 - i_cnt);
+                PORTB |= (1 << OUT_LED2);
+                PORTB &= ~(1 << OUT_LED1);
+                my_delay_us (i_cnt - 500);
+                if (i_cnt >= 1000)
+                  i_cnt = 0;
+              }
+            else
+              {
+                PORTB |= (1 << OUT_LED1);
+                PORTB &= ~(1 << OUT_LED2);
+                my_delay_us (i_cnt);
+                PORTB |= (1 << OUT_LED2);
+                PORTB &= ~(1 << OUT_LED1);
+                my_delay_us (500 - i_cnt);
+              }
+          };
+          break;
+
+        case 1:
+          {
+            PORTB |= (1 << OUT_LED1);
+            PORTB &= ~(1 << OUT_LED2);
+            if (i_cnt < 500)
+              _delay_ms (1);
+            else
+              _delay_ms (3);
+            PORTB |= (1 << OUT_LED2);
+            PORTB &= ~(1 << OUT_LED1);
+            if (i_cnt < 500)
+              _delay_ms (3);
+            else
+              _delay_ms (1);
+            if (i_cnt++ > 1000)
+              i_cnt = 0;
+          };
+          break;
+
+        case 2:
+          {
+            PORTB |= (1 << OUT_LED1);
+            PORTB &= ~(1 << OUT_LED2);
+            if (i_cnt < 500)
+              _delay_ms (0);
+            else
+              _delay_ms (2);
+            PORTB |= (1 << OUT_LED2);
+            PORTB &= ~(1 << OUT_LED1);
+            if (i_cnt < 500)
+              _delay_ms (2);
+            else
+              _delay_ms (0);
+            if (i_cnt++ > 1000)
+              i_cnt = 0;
+          };
+          break;
+        }
+      if (i_but_delay > 0)
+        i_but_delay--;
     }
-
-
 }
